de33a8dd3e9124e6ee57f14d6df676792572a844
moreira.kd@outlook.com <moreira.kd@outlook.com>
2020-06-29 15:07:39 -0400

    buttons on screen and useState hook

--- rock-paper-scissors-lizard-spock/src/App.js
+++ rock-paper-scissors-lizard-spock/src/App.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+// components
+import { Player } from './player/Player.js'
+
+
+const signs = ["rock", "paper", "scissor", "lizard", "spock"]
+
 function App() {
+
+  const state = useState({
+    playerOne: signs[0],
+    AI: signs[0],
+    winner: ""
+    
+  })
+
   return (
     <div className="App">
-      <h1>TEST</h1>
+      <h1>Rock Paper Scissor Lizard Spock</h1>
+      <Player />
+
+      <div>
+      <button className = "weaponBtn">
+        Rock
+      </button>
+      <button className = "weaponBtn">
+        Paper
+      </button>
+      <button className = "weaponBtn">
+        Scissor
+      </button>
+      <button className = "weaponBtn">
+        Lizard
+      </button>
+      <button className = "weaponBtn">
+        Spock
+      </button>
+      
+    </div>
 
     </div>
+
+
   );
 }
 
